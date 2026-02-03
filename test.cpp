3d9876ea6b7bc073d68d479b508754734466d541
three <82389319+three-o-clock@users.noreply.github.com>
2026-02-03 11:48:26 +0530

    adding  gitignore

--- test.cpp
+++ test.cpp
@@ -2,78 +2,87 @@
 using namespace std;
 
 // Definition for a binary tree node.
-struct TreeNode {
+struct TreeNode
+{
     int data;
     TreeNode *left;
     TreeNode *right;
     TreeNode(int val) : data(val), left(nullptr), right(nullptr) {}
 };
 
-class Solution {
-  public:
-    vector<int> bottomView(TreeNode *root) {
+class Solution
+{
+public:
+    vector<int> bottomView(TreeNode *root)
+    {
         // Vector to store the result
         vector<int> ans;
-        
+
         // Check if the tree is empty
-        if (root == nullptr) {
+        if (root == nullptr)
+        {
             return ans;
         }
-        
+
         // Map to store the bottom view nodes
         // based on their vertical positions
         map<int, int> mpp;
-        
+
         // Queue for BFS traversal, each
         // element is a pair containing node
         // and its vertical position
-        queue<pair<TreeNode*, int>> q;
-        
+        queue<pair<TreeNode *, int>> q;
+
         // Push the root node with its vertical
         // position (0) into the queue
         q.push({root, 0});
-        
+
         // BFS traversal
-        while (!q.empty()) {
+        while (!q.empty())
+        {
             // Retrieve the node and its vertical
             // position from the front of the queue
             auto it = q.front();
             q.pop();
-            TreeNode* node = it.first;
+            TreeNode *node = it.first;
             int line = it.second;
-            
+
             // Update the map with the node's data
             // for the current vertical position
             mpp[line] = node->data;
-            
+
             // Process left child
-            if (node->left != nullptr) {
+            if (node->left != nullptr)
+            {
                 // Push the left child with a decreased
                 // vertical position into the queue
                 q.push({node->left, line - 1});
             }
-            
+
             // Process right child
-            if (node->right != nullptr) {
+            if (node->right != nullptr)
+            {
                 // Push the right child with an increased
                 // vertical position into the queue
                 q.push({node->right, line + 1});
             }
         }
-        
+
         // Transfer values from the
         // map to the result vector
-        for (auto it : mpp) {
+        for (auto it : mpp)
+        {
             ans.push_back(it.second);
         }
-        
+
         return ans;
     }
 };
 
-int main() {
+int main()
+{
     // Creating a sample binary tree
-    TreeNode* root = new TreeNode(1);
+    TreeNode *root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->left->left = new TreeNode(4);
     root->left->right = new TreeNode(10);
@@ -90,7 +99,8 @@ int main() {
 
     // Print the result
     cout << "Bottom View Traversal: " << endl;
-    for (auto node : bottomView) {
+    for (auto node : bottomView)
+    {
         cout << node << " ";
     }
 
