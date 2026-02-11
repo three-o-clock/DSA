72b9dbe348062e030f6006ff29f9f80b582bc736
three <82389319+three-o-clock@users.noreply.github.com>
2026-02-11 22:20:09 +0530

    11/02

--- trees/maximumWidthofBT.cpp
+++ trees/maximumWidthofBT.cpp
@@ -17,33 +17,43 @@ public:
     long long int widthOfBinaryTree(TreeNode *root)
     {
         // your code goes here
-        queue<TreeNode *> q;
-        vector<TreeNode *> v;
 
-        q.push(root);
-        q.push(NULL);
+        if (!root)
+            return 0;
+        queue<pair<TreeNode *, int>> q;
+
+        int ans = 0;
+
+        q.push({root, 0});
 
         while (!q.empty())
         {
-            TreeNode *current = q.front();
-            v.push_back(current);
-            q.pop();
+            int n = q.size();
+            int leftMostIdx = q.front().second;
 
-            if (current == nullptr)
-            {
-                if (!q.empty())
-                {
-                    q.push(NULL);
-                }
-            }
+            int first, last;
 
-            else
+            for (int i = 0; i < n; i++)
             {
+                TreeNode *current = q.front().first;
+                int currIdx = q.front().second - leftMostIdx; // index of nodes in a level relative to the leftmost node
+
+                q.pop();
+
+                if (i == 0)
+                    first = currIdx;
+                if (i == n - 1)
+                    last = currIdx;
+
                 if (current->left)
-                    q.push(current->left);
+                    q.push({current->left, (2 * currIdx) + 1});
                 if (current->right)
-                    q.push(current->right);
+                    q.push({current->right, (2 * currIdx) + 2});
             }
+
+            ans = max(ans, last - first + 1);
         }
+
+        return ans;
     }
 };
